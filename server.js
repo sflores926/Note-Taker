28875bb94e395c790e25c3589580cc4ab5a1c8c8
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-09-08 14:07:25 -0400

    created to js files in route folder and worked on server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// dependencies
+const express = require('express');
+
+// Route files
+const apiRoutes = require('./routes/apiRoutes');
+const indexRoutes = require('./routes/indexRoutes');
+
+// Set port
+const PORT = process.env.PORT || 3001;
+
+//initialize express
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', indexRoutes);
+
+//App Listener
+app.listen(PORT, () =>
+  console.log(`App listening at localhost:${PORT}`)
+);
