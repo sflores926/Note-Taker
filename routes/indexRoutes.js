e98ae9a91707d6f9e15b2fb8d05ecb0772d703fb
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-09-12 12:57:17 -0400

    made chages to indexRoutes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,21 +1,30 @@
 //dependencies
-const path = require ('path');
-const router = require('express').Router();
+// const path = require ('path');
+// const router = require('express').Router();
+const express = require('express');
+
+// Import our modular router
+const apiRouter = require('./apiRoutes');
+
+const app = express();
+
+app.use('/api', apiRouter);
 
 
 
 //routes to notes/db.json/main page
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
+// router.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/notes.html"));
+// });
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// router.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// router.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 
-module.exports = router;
+// module.exports = router;
+module.exports = app;
