ea27a0847077abe9fefefc81358463046d01ed95
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-09-12 14:09:15 -0400

    fixed miss word in server.js line 30 needed to add notes after the / to connect to notes.html

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ app.use(express.static('public'));
 // app.use('/api', apiRoutes);
 // app.use('/', indexRoutes);
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 //Route for note page
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public//notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 //Route to redirect to homepage
